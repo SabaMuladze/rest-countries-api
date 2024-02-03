0cea764f031cf1e177a0dae75ab64ac745f7f44a
SabaMuladze <muladze66@bk.ru>
2024-02-03 23:51:48 +0400

    add filter on input

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,36 @@
 <template>
   <div :class="{ dark: dark }">
-    <section
-      class="min-h-[100vh] w-full flex flex-wrap justify-center gap-[36px] bg-[#fafafa] dark:bg-[#202C36] md:gap-[56px]"
-    >
+    <section class="min-h-[100vh] w-full bg-[#fafafa] dark:bg-[#202C36]">
+      <div class="w-full px-4 py-3">
+        <div
+          class="px-4 py-3 bg-white rounded-md dark:bg-[#2B3844] md:w-[480px]"
+        >
+          <input
+            v-model="val"
+            placeholder="Search for a country…"
+            class="bg-white dark:bg-[#2B3844] w-full dark:text-white outline-none"
+            type="text"
+            @input="filterCountries"
+          />
+        </div>
+      </div>
       <div
-        class="w-[264px] h-[336px] bg-white mt-6 rounded-sm dark:bg-[#2B3844] hover:scale-110 transition-all"
-        v-for="(country, index) in cardData"
-        :key="country.name"
+        class="w-full flex flex-wrap justify-center gap-[36px] bg-[#fafafa] dark:bg-[#202C36] md:gap-[56px]"
       >
-        <img class="h-[160px] w-full" :src="country?.flags.png" alt="" />
-        <div class="flex flex-col p-6">
-          <h2 class="mb-[20px]">{{ country.name }}</h2>
-          <p class="leading-7">Population: {{ country.population }}</p>
-          <p class="leading-7">Region: {{ country.region }}</p>
-          <p class="leading-7">Capital: {{ country.capital }}</p>
+        <div
+          class="w-[264px] h-[336px] bg-white mt-6 rounded-md cursor-pointer dark:bg-[#2B3844] hover:scale-110 transition-all"
+          v-for="(country, index) in originalData"
+          :key="country.name"
+        >
+          <img class="h-[160px] w-full" :src="country?.flags.png" alt="" />
+          <div class="flex flex-col justify-between p-6">
+            <h2 class="mb-[10px]">{{ country.name }}</h2>
+            <div>
+              <p class="leading-6">Population: {{ country.population }}</p>
+              <p class="leading-6">Region: {{ country.region }}</p>
+              <p class="leading-6">Capital: {{ country.capital }}</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
@@ -27,9 +44,21 @@ export default {
   data() {
     return {
       cardData: data,
+      originalData: [],
+      val: "",
     };
   },
-  methods: {},
+  methods: {
+    filterCountries() {
+      if (this.val.length > 0) {
+        this.originalData = this.cardData.filter((country) =>
+          country.name.toLowerCase().includes(this.val)
+        );
+      } else {
+        this.originalData = this.cardData;
+      }
+    },
+  },
   computed: {
     ...mapGetters(["darkMode"]),
     dark() {
@@ -37,7 +66,7 @@ export default {
     },
   },
   mounted() {
-    console.log(this.cardData);
+    this.originalData = this.cardData;
   },
 };
 </script>
--- src/components/Inner.vue
+++ src/components/Inner.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>Inner</h1>
+  <div :class="{ dark: dark }">
+    <section
+      class="w-full h-[90vh] flex flex-col bg-[#fafafa] p-6 dark:bg-[#202C36]"
+    >
+      <button
+        class="py-2 px-3 shadow-md bg-white flex justify-between items-center gap-2 dark:bg-[#2B3844]"
+      >
+        <i class="fa-solid fa-arrow-left"></i>
+        <p>Back</p>
+      </button>
+    </section>
+  </div>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["darkMode"]),
+    dark() {
+      return this.darkMode;
+    },
+  },
+};
+</script>
